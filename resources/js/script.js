71ee38df67621309cb0a174d61976a72595fbd5a
omarkusan <omarkusan@gmail.com>
2017-05-14 23:07:36 -0500

    adding google map

--- resources/js/script.js
+++ resources/js/script.js
@@ -81,9 +81,17 @@ $(document).ready(function() {
         
     });
     /*Maps*/
-    new GMaps({
+   var map = new GMaps({
   div: '.map',
   lat: -12.043333,
   lng: -77.028333
+});
+    map.addMarker({
+  lat: -12.043333,
+  lng: -77.028333,
+  title: 'Lima',
+  infoWindow: {
+  content: '<p>My Lisbon HQ</p>'
+}
 });
 });
